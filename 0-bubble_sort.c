f38f713bf129dacb25cd2055d89b6e2f475038ca
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-24 13:41:02 -0800

    0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting algorithm that sorts in form
+ * of a bubble
+ *
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Description_of_the_Repository
